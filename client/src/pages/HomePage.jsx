faed701f9e42d5ce769ef74e49f387bc44c4988c
Gokusan453 <gokhangunerr4@gmail.com>
2025-06-18 18:01:26 +0200

    HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Welcome to my Kanban app!</h1>
-      <p>Please select an item from the navigation menu.</p>
+      <p>Please select an item from the menu.</p>
     </div>
   );
 }
